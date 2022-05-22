6c14a3e1c05aa77f935bf27bce2d00cbee639155
Max <95238323+tykhonenkomax@users.noreply.github.com>
2022-05-23 00:03:53 +0300

    Lesson3_resolve 2 21:49

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
+import {MainLayoutComponent} from "./layouts/main-layout/main-layout.component";
+import {UsersComponent} from "../modules/users/components/users/users.component";
+import {PostsModule, UsersModule} from "../modules";
 
 const routes:Routes =[
+  {
+    path:'',component:MainLayoutComponent, children:[
+      {path:'',redirectTo:'users',pathMatch:'full'},
+      {path:'users',loadChildren:()=>import('./app.module').then(value => UsersModule)},
+      {path:'posts',loadChildren:()=>import('.././modules/posts/posts.module').then(value => PostsModule )},
+      {path:'comments',loadChildren:()=>import('.././modules/comments/comments.module').then(value => CommonModule)}
+    ]
 
+  }
 ]
 
 
