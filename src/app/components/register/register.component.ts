ac8b38d6520265429d7c3b41abfa16035526e72a
Max <95238323+tykhonenkomax@users.noreply.github.com>
2022-05-25 17:59:01 +0300

    Lesson5 дороблений валідатор

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -17,10 +17,10 @@ export class RegisterComponent implements OnInit {
   }
 _createForm():void{
     this.form = new FormGroup({
-      username: new FormControl(null, [Validators.minLength(2),Validators.maxLength(20)]),
-      password: new FormControl(null, [Validators.minLength(2),Validators.maxLength(20)]),
-      confirmPassword: new FormControl(null, [Validators.minLength(2),Validators.maxLength(20)])
-    })
+      username: new FormControl(null, [Validators.required, Validators.minLength(2),Validators.maxLength(20)]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(2),Validators.maxLength(20)]),
+      confirmPassword: new FormControl(null, [Validators.required, Validators.minLength(2),Validators.maxLength(20)])
+    },[this._checkPasswords])
 }
 
 
