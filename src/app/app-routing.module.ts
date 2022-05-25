d8c40c5f2af73c44549ef77b13ce3691008eaa9b
Max <95238323+tykhonenkomax@users.noreply.github.com>
2022-05-25 22:33:23 +0300

    Lesson5 double 1 start

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const routes:Routes=[
+
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers:[]
+})
+export class AppRoutingModule { }
