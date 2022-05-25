575c67d795992a566cd89041503a707025f1e1b7
Max <95238323+tykhonenkomax@users.noreply.github.com>
2022-05-26 00:21:38 +0300

    Lesson5 double 2 лекція 4,53

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import { MainComponent } from './components/main/main.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { CarComponent } from './components/car/car.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrComponent } from './components/register/registr.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CarsComponent,
+    CarComponent,
+    LoginComponent,
+    RegistrComponent,
+    HeaderComponent,
+    MainLayoutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
